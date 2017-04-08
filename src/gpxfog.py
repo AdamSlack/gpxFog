ea4646d8822d5b8a8cd0f7eb7940f2e0f7fd45a4
Adam Slack <AdamSlack@outlook.com>
2017-04-08 14:07:28 +0100

    web stuff started

--- src/gpxfog.py
+++ src/gpxfog.py
@@ -1,33 +1,6 @@
 import gpxpy, os, mplleaflet
 import matplotlib.pyplot as plt
-
-def parseGPX(dir):
-    # reading GPX files.
-    fileList = os.listdir(dir)
-    gpxData = []
-
-    for f in fileList:
-        gpx = open(dir + '/' + f, 'r')
-        gpxData.append(gpxpy.parse(gpx))
-
-    print(dir + ' Parsed')
-    lat = []
-    lon = []
-
-    # plot data
-    for gpx in gpxData:
-
-        for track in gpx.tracks:
-            for segment in track.segments:
-                for point in segment.points:
-                    lat.append(point.latitude)
-                    lon.append(point.longitude)
-
-    return lat, lon
-
-def drawGPX(lat, lon, colour):
-    plt.plot(lon, lat, color=colour, lw=1, alpha=1)
-
+import time
 
 def plotGPX(dir, colour):
     # reading GPX files.
@@ -46,11 +19,12 @@ def plotGPX(dir, colour):
 
         for track in gpx.tracks:
             for segment in track.segments:
-                for point in segment.points:
-                    lat.append(point.latitude)
-                    lon.append(point.longitude)
 
-        plt.plot(lon, lat, color=colour, lw=3, alpha=0.8)
+                for i in range(0, len(segment.points), 10):
+                    lat.append(segment.points[i].latitude)
+                    lon.append(segment.points[i].longitude)
+
+        plt.plot(lon, lat, color=colour, lw=3, alpha=0.5)
     print(dir + ' plotted')
 
 # init plot
@@ -59,15 +33,15 @@ ax = plt.Axes(fig, [0., 0., 1., 1.], )
 ax.set_aspect('equal')
 ax.set_axis_off()
 fig.add_axes(ax)
-
 # show plot
-plotGPX('../adamGPX', 'blue')
 plotGPX('../joeGPX', 'red')
 plotGPX('../gabGPX', 'mediumvioletred')
 plotGPX('../scoGPX', 'orange')
+plotGPX('../adamGPX', 'lawngreen')
 
-mplleaflet.show(fig,)
+
+mplleaflet.save_html(fig, '../maps/'+str(int(time.time())) + '.html')
 
 plt.show()
 
-plt.savefig('output.png', facecolor=fig.get_facecolor(), bbox_inches='tight', pad_inches=0, dpi=500)
+# plt.savefig('output.png', facecolor=fig.get_facecolor(), bbox_inches='tight', pad_inches=0, dpi=500)
